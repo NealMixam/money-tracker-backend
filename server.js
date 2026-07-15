d2499e28fa45e46338ee293539843dc7d35661b5
NealMixam <maksimilianrogulin@gmail.com>
2026-07-16 00:10:54 +0300

    Добавлена проверка невалидного JSON в запросе

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-const { createServer } = require('node:http');
+const { createServer } = require("node:http");
 
 const PORT = process.env.PORT || 3000;
 
-const server = createServer((req, res)  => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ ok: true }));
+const server = createServer((req, res) => {
+  if (req.method === "POST" && req.url === "/echo") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      let data;
+      try {
+        data = JSON.parse(body);
+      } catch (error) {
+        res.statusCode = 400;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ error: "Invalid JSON" }));
+        return;
+      }
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(data));
+    });
+  }
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
